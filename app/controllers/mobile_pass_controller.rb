55412bb002781dc846805e48a78b2647ae1d26cb
Troy Anderson <troy@alliedcode.com>
2023-06-18 13:21:39 -0700

    Added current_user and implemented authenticate_user!

--- app/controllers/mobile_pass_controller.rb
+++ app/controllers/mobile_pass_controller.rb
@@ -2,20 +2,31 @@ class MobilePassController < MobilePass.parent_controller.constantize
   rescue_from MobilePass::Error, with: :handle_exception
 
   if respond_to?(:helper_method)
-    helpers = %w[authenticate_user!]
+    helpers = %w[current_user authenticate_user!]
     helper_method(*helpers)
   end
 
   protected
 
-  def authenticate_user!
-    # TODO
+  def current_user
+    @_current_user ||= validate_auth_token.user if validate_auth_token.success?
   end
 
+  # Ignoring :reek:MissingSafeMethod
+  def authenticate_user!
+    fail!(:authentication, validate_auth_token.code, validate_auth_token.message) if validate_auth_token.failure?
+  end
+
+  def validate_auth_token
+    @_validate_auth_token ||= MobilePass::ValidateAuthToken.call auth_token: headers['X-Auth']
+  end
+
+  # Ignoring :reek:MissingSafeMethod
   def fail!(context, code, message)
     raise MobilePass::Error.new(context, code, message)
   end
 
+  # Ignoring :reek:UncommunicativeParameterName
   def handle_exception(e)
     render json: { error: { context: e.context, code: e.code, message: e.message } }, status: :unprocessable_entity
   end
