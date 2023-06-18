65cbb8fa3ed78fe8bfd08c67d4721e2866a538eb
Troy Anderson <troy@alliedcode.com>
2023-06-17 22:47:01 -0700

    Initial gem generation.

---
+++ lib/mobile_pass.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mobile_pass/version"
+
+module MobilePass
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mobile_pass_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MobilePass do
+  it "has a version number" do
+    expect(MobilePass::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
