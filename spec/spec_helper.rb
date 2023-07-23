460aa7de6780ba0ff8b65b3ada58dd119d8a6e09
Troy Anderson <troy@alliedcode.com>
2023-07-22 17:38:45 -0700

    Fixed symbol name.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ end
 
 if ENV['CI'] == 'true'
   require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter.codecov
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
 RSpec.configure do |config|
